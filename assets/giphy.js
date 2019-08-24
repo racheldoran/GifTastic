9515e112e6ea4575784ae5a5dadec1d0aad4c2d0
rachel doran <rmitchell0925@gmail.com>
2019-08-24 14:35:36 -0400

    modify

--- assets/giphy.js
+++ assets/giphy.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 });
 
 
-var gif = ["Bob Dylan", "Waylon Jennings", "Marshall Tucker Band", "The Doobie Brothers"];
+var gif = ["Bob Dylan", "Waylon Jennings", "Tom Petty"];
 
 
 // Function for dumping the JSON content for each button into the div
@@ -41,20 +41,47 @@ $("button").on("click", function() {
           
           renderButtons();
         }
-       
+      
       })
+    })
 
-      
-      ;
-      
+    var displayGif = $("#gif-input")
 
-      function renderButtons() {
+    function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise we will have repeat buttons)
-        $("#buttons-view").empty();
+      // Deleting the movies prior to adding new movies
+      // (this is necessary otherwise we will have repeat buttons)
+      $("#gif-input").empty();
 
-  };
-  
-})
-  
+      // Looping through the array of movies
+      for (var i = 0; i < displayGif.length; i++) {
+
+        // Then dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of movie to our button
+        a.addClass("gif");
+        // Adding a data-attribute
+        a.attr("data-name", displayGif[i]);
+        // Providing the initial button text
+        a.text(displayGif[i]);
+        // Adding the button to the HTML
+        $("#buttons-view").append(a);
+      }
+    }
+
+
+      // This function handles events where one button is clicked
+      $("#add-gif").on("click", function(event) {
+        // Preventing the buttons default behavior when clicked (which is submitting a form)
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var giffy = $("#gif-input").val().trim();
+
+        // Adding the movie from the textbox to our array
+        giffy.push(gif);
+
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+
+      });
