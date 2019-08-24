848e3af67627d1cb25308a650fc134b17dc8ce4a
rachel doran <rmitchell0925@gmail.com>
2019-08-24 15:03:23 -0400

    modify

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    console.log("yooooo girl!");
-});
+  console.log("yooooo girl!");
 
 
-var gif = ["Bob Dylan", "Waylon Jennings", "Tom Petty"];
+
+  var gif = ["Bob Dylan", "Waylon Jennings", "Tom Petty"];
 
 
-// Function for dumping the JSON content for each button into the div
-$("button").on("click", function() {
+  // Function for dumping the JSON content for each button into the div
+  $("button").on("click", function () {
     var person = $(this).attr("data-person");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       person + "&api_key=MuglAHZY6tEW36wfJ2k6Alpr9UM1eaKg&limit=10";
-     console.log(queryURL)
+    console.log(queryURL)
 
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      .then(function(response) {
+      .then(function (response) {
         var results = response.data;
         console.log(response)
         console.log(results)
@@ -38,57 +38,38 @@ $("button").on("click", function() {
           gifDiv.prepend(personImage);
 
           $("#gifs-appear-here").prepend(gifDiv);
-          
-          renderButtons();
+
         }
-      
+
       })
-    })
+  })
 
-    var displayGif = ["Bob Dylan"];
 
-    // Generic function for capturing the movie name from the data-attribute
-    function alertgifName() {
-      var displayGif = $(this).attr("data-name");
-      
+  var displayGif = $("#<div>")
+
+  // Function for displaying movie data
+  function renderButtons() {
+
+
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < displayGif.length; i++) {
+
+      var a = $("<button>");
+      a.addClass("gif");
+      a.attr("data-name", displayGif[i]);
+      a.text(displayGif[i])
+      $("#buttons-view").append(a);
     }
+  }
 
-    // Function for displaying movie data
-    function renderButtons() {
+  $("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    var giffy = $("#gif-input").val().trim();
+    displayGif.push(giffy);
 
-      // Deleting the movies prior to adding new movies
-      // (this is necessary otherwise we will have repeat buttons)
-      $("#buttons-view").empty();
+    renderButtons();
 
-      // Looping through the array of movies
-      for (var i = 0; i < displayGif.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class of movie to our button
-        a.addClass("gif");
-        // Adding a data-attribute
-        a.attr("data-name", displayGif[i]);
-        // Providing the initial button text
-        a.text(displayGif[i]);
-        // Adding the button to the HTML
-        $("#buttons-view").append(a);
-      }
-    }
-
-    // This function handles events where one button is clicked
-    $("#add-movie").on("click", function(event) {
-      // Preventing the buttons default behavior when clicked (which is submitting a form)
-      event.preventDefault();
-      // This line grabs the input from the textbox
-      var giffy = $("#gif-input").val().trim();
-
-      // Adding the movie from the textbox to our array
-      displayGif.push(giffy);
-
-      // Calling renderButtons which handles the processing of our movie array
-      renderButtons();
-
-    });
-  
+  });
+  renderButtons();
+});
