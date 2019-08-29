1159ab7378f4bfe1c2a7d178f96e26f5fb8c7a23
rachel doran <rmitchell0925@gmail.com>
2019-08-28 21:22:19 -0400

    modify

--- assets/giphy.js
+++ assets/giphy.js
@@ -72,7 +72,7 @@ $(document).ready(function () {
   });
 
 
-  $(document).on("click", ".gif", function () {
+  $(document).on("click", "img", function () {
     var state = $(this).attr("data-state");
     console.log("State: " + state)
     if (state === "still") {
